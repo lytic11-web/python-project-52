5744222eeaba30332e3d95f41604143a1ed3a884
lytic11 <lytic11@gmail.com>
2026-07-13 16:06:57 +0300

    Исправить рендеринг поля executor в TaskForm для автотестов

--- tasks/forms/task_form.py
+++ tasks/forms/task_form.py
@@ -1,8 +1,17 @@
 from django import forms
+from django.contrib.auth.models import User
 from tasks.models import Task
 
 
 class TaskForm(forms.ModelForm):
+    # Явно объявляем поле, чтобы контролировать его поведение
+    executor = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        label='Исполнитель',
+        required=False,
+        empty_label='---------'
+    )
+
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
@@ -10,10 +19,13 @@ class TaskForm(forms.ModelForm):
             'name': 'Имя',
             'description': 'Описание',
             'status': 'Статус',
-            'executor': 'Исполнитель',  # ← Django сам создаст поле с этой меткой
             'labels': 'Метки',
         }
         widgets = {
             'description': forms.Textarea(attrs={'rows': 5, 'class': 'form-control'}),
             'labels': forms.SelectMultiple(attrs={'class': 'form-select', 'size': '5'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['executor'].label_from_instance = lambda obj: obj.username

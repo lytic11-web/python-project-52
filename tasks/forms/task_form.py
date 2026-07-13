65ef897e7e7c06c8c73f8810eb925cf6708e7164
lytic11 <lytic11@gmail.com>
2026-07-13 16:20:00 +0300

    Упростить TaskForm: использовать стандартный Meta с явным виджетом Select

--- tasks/forms/task_form.py
+++ tasks/forms/task_form.py
@@ -1,17 +1,8 @@
 from django import forms
-from django.contrib.auth.models import User
 from tasks.models import Task
 
 
 class TaskForm(forms.ModelForm):
-    # Явно объявляем поле, чтобы контролировать его поведение
-    executor = forms.ModelChoiceField(
-        queryset=User.objects.all(),
-        label='Исполнитель',
-        required=False,
-        empty_label='---------'
-    )
-
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
@@ -19,13 +10,12 @@ class TaskForm(forms.ModelForm):
             'name': 'Имя',
             'description': 'Описание',
             'status': 'Статус',
+            'executor': 'Исполнитель',
             'labels': 'Метки',
         }
         widgets = {
             'description': forms.Textarea(attrs={'rows': 5, 'class': 'form-control'}),
+            # Явно указываем виджет Select
+            'executor': forms.Select(attrs={'class': 'form-select'}),
             'labels': forms.SelectMultiple(attrs={'class': 'form-select', 'size': '5'}),
         }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['executor'].label_from_instance = lambda obj: obj.username

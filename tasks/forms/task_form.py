0d34f8e3ba932c3bad0472aee3c2003ca505c93d
lytic11 <lytic11@gmail.com>
2026-07-14 18:55:39 +0300

    change tasks/forms/task_form.py

--- tasks/forms/task_form.py
+++ tasks/forms/task_form.py
@@ -1,15 +1,7 @@
 from django import forms
-from django.contrib.auth.models import User
 from tasks.models import Task
 
-
 class TaskForm(forms.ModelForm):
-    executor = forms.ModelChoiceField(
-        queryset=User.objects.all(),
-        label='Исполнитель',
-        required=False
-    )
-
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
@@ -17,13 +9,10 @@ class TaskForm(forms.ModelForm):
             'name': 'Имя',
             'description': 'Описание',
             'status': 'Статус',
+            'executor': 'Исполнитель',
             'labels': 'Метки',
         }
         widgets = {
             'description': forms.Textarea(attrs={'rows': 5, 'class': 'form-control'}),
             'labels': forms.SelectMultiple(attrs={'class': 'form-select', 'size': '5'}),
         }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['executor'].label_from_instance = lambda obj: obj.username
